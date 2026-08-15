9d415d34dbf633ef80027bdc9997ffbe54269060
Ahmed Raafat <your-email@example.com>
2026-08-15 15:51:58 +0300

    Add AdSense ownership meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-7982982112293185"
+        />
+
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7982982112293185"
